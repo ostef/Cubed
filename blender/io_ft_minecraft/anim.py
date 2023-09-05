8ab7e7f56c99fd5ed2e38d7d396dbcb2f6b30c7f
Stefan <47391841+ostef@users.noreply.github.com>
2023-09-05 23:57:54 +0200

    Mesh and anim exporter

---
+++ blender/io_ft_minecraft/anim.py
@@ -0,0 +1,238 @@
+import bpy
+import mathutils
+
+from typing import (
+    List,
+    Dict,
+    Tuple
+)
+
+from bpy.props import (
+    IntProperty,
+    BoolProperty,
+    EnumProperty
+)
+
+from bpy_extras.io_utils import (
+    ExportHelper,
+    orientation_helper,
+    axis_conversion
+)
+
+class JointSample:
+    def __init__ (
+        self,
+        local_position : Tuple[float, float, float],
+        local_orientation : Tuple[float, float, float, float],
+        local_scale : Tuple[float, float, float]
+    ):
+        self.local_position = local_position
+        self.local_orientation = local_orientation
+        self.local_scale = local_scale
+
+class JointAnimation:
+    def __init__ (
+        self,
+        name : str
+    ):
+        self.name = name
+        self.samples : List[JointSample] = []
+
+class SampledAnimation:
+    def __init__ (
+        self
+    ):
+        self.sample_count : int = 0
+        self.name_to_joint_id : Dict[str, int] = {}
+        self.joints : List[JointAnimation] = []
+
+    def FromAction (
+        blender_obj : bpy.types.Object,
+        blender_action : bpy.types.Action,
+        frame_begin : int,
+        frame_end : int,
+        frame_step : int,
+        transform_matrix : mathutils.Matrix
+    ):
+        def AppendPose (
+            anim : SampledAnimation,
+            pose : bpy.types.Pose
+        ):
+            for bone in pose.bones:
+                # @Note (stefan): Is it possible for a bone to
+                # spawn in the middle of an animation ? We don't
+                # want that.
+                if bone.name not in anim.name_to_joint_id:
+                    continue
+
+                matrix = transform_matrix @ bone.matrix
+
+                if bone.parent is not None:
+                    parent_matrix = transform_matrix @ bone.parent.matrix
+                    matrix = parent_matrix.inverted () @ matrix
+
+                location, orientation, scale = matrix.decompose ()
+                joint_index = anim.name_to_joint_id[bone.name]
+
+                anim.joints[joint_index].samples.append (
+                    JointSample (
+                        tuple (location),
+                        (
+                            orientation[1],
+                            orientation[2],
+                            orientation[3],
+                            orientation[0]
+                        ),
+                        tuple (scale)
+                    )
+                )
+
+        result = SampledAnimation ()
+        prev_action = blender_obj.animation_data.action
+        prev_frame  = bpy.context.scene.frame_current
+        blender_obj.animation_data.action = blender_action
+
+        # Initialize the joint animations array and name dict
+        bpy.context.scene.frame_set (frame_begin)
+        for bone in blender_obj.pose.bones:
+            if not bone.bone.use_deform:
+                continue
+
+            joint_index = len (result.joints)
+            joint_anim = JointAnimation (bone.name)
+            result.joints.append (joint_anim)
+            result.name_to_joint_id.update ({ bone.name : joint_index })
+
+        # Go through each frame in the animation and add the pose to the anim
+        for frame in range (frame_begin, frame_end, frame_step):
+            bpy.context.scene.frame_set (frame)
+            AppendPose (result, blender_obj.pose)
+
+        bpy.context.scene.frame_set (prev_frame)
+        blender_obj.animation_data.action = prev_action
+
+        # Make sure we have the same number of samples for each joint
+        if len (result.joints) > 0:
+            result.sample_count = len (result.joints[0].samples)
+
+        for joint in result.joints:
+            if len (joint.samples) != result.sample_count:
+                raise Exception (f"Inconsistent sample count for joints in animation {blender_action.name}.")
+
+        return result
+
+    def WriteText (self, filename : str):
+        with open (filename, "wb") as file:
+            fw = file.write
+            fw (b"[1]\n\n")	# Version
+            fw (b"joint_count %u\n" % len (self.joints))
+            fw (b"sample_count %u\n\n" % self.sample_count)
+
+            for joint in self.joints:
+                fw (b"%s\n\n" % bytes (joint.name, 'UTF-8'))
+
+                for sample in joint.samples:
+                    fw (b"%.6f %.6f %.6f\n" % sample.local_position[:])
+                    fw (b"%.6f %.6f %.6f %.6f\n" % sample.local_orientation[:])
+                    fw (b"%.6f %.6f %.6f\n\n" % sample.local_scale[:])
+
+def ExportAnimations (
+    context : bpy.types.Context,
+    filename : str,
+    use_action_frame_range : bool,
+    frame_step : int,
+    use_selection : bool,
+    apply_transform : bool,
+    axis_conversion_matrix : mathutils.Matrix
+):
+    import os
+
+    if bpy.ops.object.mode_set.poll ():
+        bpy.ops.object.mode_set (mode = 'OBJECT')
+
+    if use_selection:
+        objs = context.selected_objects
+    else:
+        objs = context.scene.objects
+
+    exported_actions : List[bpy.types.Action] = []
+    for obj in objs:
+        if obj.animation_data is None or obj.pose is None:
+            continue
+
+        action = obj.animation_data.action
+        if action is None or action in exported_actions:
+            continue
+
+        transform_matrix = mathutils.Matrix.Identity (4)
+        if apply_transform:
+            transform_matrix = transform_matrix @ obj.matrix_world
+        if axis_conversion_matrix is not None:
+            transform_matrix = transform_matrix @ axis_conversion_matrix.to_4x4 ()
+
+        output_filename = os.path.join (os.path.dirname (filename), action.name) + Exporter.filename_ext
+        if use_action_frame_range:
+            frame_begin, frame_end = (
+                int (action.frame_range[0]),
+                int (action.frame_range[1])
+            )
+        else:
+            frame_begin, frame_end = (
+                int (context.scene.frame_start),
+                int (context.scene.frame_end)
+            )
+
+        anim = SampledAnimation.FromAction (obj, action, frame_begin, frame_end, frame_step, transform_matrix)
+        anim.WriteText (output_filename)
+        print (f"Exported animation clip {action.name} to file {output_filename}.\n")
+        exported_actions.append (action)
+
+@orientation_helper (axis_forward = '-Z', axis_up = 'Y')
+class Exporter (bpy.types.Operator, ExportHelper):
+    """Export animation data"""
+    bl_idname = "export.anim_example_anim"
+    bl_label = "Export sampled animation (.anim)"
+    bl_options = { 'REGISTER', 'UNDO' }
+    filename_ext = ".anim"
+
+    use_selection : BoolProperty (
+        name = "Only Selected",
+        description = "Export only the active action of the selected objects.",
+        default = True
+    )
+
+    apply_transform : BoolProperty (
+        name = "Apply object transform",
+        description = "Apply the object transform matrix when exporting animations.",
+        default = True
+    )
+
+    use_action_frame_range : BoolProperty (
+        name = "Use action frame range",
+        description = "Use the action frame range rather than the scene frame range.",
+        default = False
+    )
+
+    frame_step : IntProperty (
+        name = "Frame step",
+        description = "How many frames to advance when sampling the animation.",
+        default = 1
+    )
+
+    def execute (self, context : bpy.types.Context):
+        context.window.cursor_set ('WAIT')
+        ExportAnimations (
+            context,
+            self.filepath,
+            self.use_action_frame_range,
+            self.frame_step,
+            self.use_selection,
+            self.apply_transform,
+            axis_conversion (to_forward = self.axis_forward, to_up = self.axis_up)
+        )
+        context.window.cursor_set ('DEFAULT')
+
+        return { 'FINISHED' }
+
+def export_menu_func (self, context : bpy.types.Context):
+    self.layout.operator (Exporter.bl_idname)
---
+++ blender/io_ft_minecraft/mesh.py
@@ -0,0 +1,290 @@
+import bpy
+import bmesh
+import mathutils
+
+from typing import (
+    List,
+    Dict,
+    Tuple
+)
+
+from bpy.props import (
+    StringProperty,
+    BoolProperty,
+    FloatProperty
+)
+
+from bpy_extras.io_utils import (
+    ExportHelper,
+    orientation_helper,
+    axis_conversion
+)
+
+class Vertex:
+    def __init__ (
+        self,
+        position : Tuple[float, float, float],
+        normal   : Tuple[float, float, float],
+        tex_coords : Tuple[float, float],
+        joint_id : int,
+    ):
+        self.position = position
+        self.normal = normal
+        self.tex_coords = tex_coords
+        self.joint_id = joint_id
+
+class Mesh:
+    def __init__ (
+        self
+    ):
+        self.name_to_joint_id : Dict[str, int] = {}
+        self.joints : List[bpy.types.Bone] = []
+        self.verts : List[Vertex] = []
+        self.tris : List[Tuple[int, int, int]] = []
+
+    def FromMeshAndArmature (
+        blender_obj : bpy.types.Object,
+        blender_mesh : bpy.types.Mesh,
+        blender_armature : bpy.types.Armature
+    ):
+        def AppendHierarchy (joints : List[bpy.types.Bone], bone : bpy.types.Bone):
+            joints.append (bone)
+
+            for child in bone.children:
+                if child.use_deform:
+                    AppendHierarchy (joints, child)
+
+        result = Mesh ()
+
+        if blender_armature is not None:
+            # Fill skeleton data
+            root = None
+
+            # Find the root bone
+            for b in blender_armature.bones:
+                if b.parent is None and b.use_deform:
+                    if root is not None:
+                        raise Exception ("Found multiple root bones in armature.")
+                    root = b
+
+            if root is None:
+                raise Exception ("Could not find root bone.")
+
+            AppendHierarchy (result.joints, root)
+
+            if len (result.joints) > 0x7fff:
+                raise Exception (f"Armature has { len (result.joints) } bones, which is more than the maximum allowed ({0x7fff}).")
+
+            for i, b in enumerate (result.joints):
+                result.name_to_joint_id.update ({ b.name : i })
+
+        # Fill vertex and triangle data
+        vert_group_names = { g.index : g.name for g in blender_obj.vertex_groups }
+        vertices_dict = {}
+        for i, poly in enumerate (blender_mesh.polygons):
+            if len (poly.vertices) != 3:
+                raise Exception ("Mesh has polygons that are not triangles. Make sure to triangulate the mesh prior.")
+
+            tri = []
+            for j, vert_index in enumerate (poly.vertices):
+                if vert_index in vertices_dict:
+                    result_vert_index = vertices_dict[vert_index]
+                else:
+                    result_vert_index = len (result.verts)
+                    vertices_dict.update ({ vert_index : result_vert_index })
+                    vert = blender_mesh.vertices[vert_index]
+
+                    if len (vert.groups) != 0 and blender_armature is None:
+                        raise Exception ("Mesh has vertices assigned to vertex groups, but we could not find an armature associated with it. Make sure it is parented to an armature, or it has a valid skin modifier.")
+
+                    if len (vert.groups) > 1:
+                        raise Exception (f"Vertex {vert_index} has more than 1 group assigned to it.")
+
+                    joint_id = -1
+                    if len (vert.groups) > 0:
+                        group = vert.groups[0].group
+                        name = vert_group_names[group]
+                        if name not in result.name_to_joint_id:
+                            raise Exception (f"Vertex is assigned to group {name} but we could not find a deform bone with this name in the armature.")
+                        joint_id = result.name_to_joint_id[name]
+
+                    result.verts.append (Vertex (
+                        tuple (vert.co),
+                        tuple (vert.normal),
+                        [0,0],
+                        joint_id
+                    ))
+
+                tri.append (result_vert_index)
+
+            result.tris.append ((tri[0], tri[1], tri[2]))
+
+        return result
+
+    def WriteBinary (self, filename : str):
+        import struct
+
+        with open (filename, "wb") as file:
+            fw = file.write
+            fw (b"SKINNED_MESH")
+            fw (struct.pack ("<I", 10000)) # File version
+
+            fw (struct.pack ("<I", len (self.verts)))
+            fw (struct.pack ("<I", len (self.tris)))
+
+            for vert in self.verts:
+                fw (struct.pack ("<fff", *vert.position))
+                fw (struct.pack ("<fff", *vert.normal))
+                fw (struct.pack ("<ff", *vert.tex_coords))
+                fw (struct.pack ("<h", vert.joint_id))
+                fw (struct.pack ("xx"))
+
+            for tri in self.tris:
+                fw (struct.pack ("<III", *tri))
+
+            fw (struct.pack ("<h", len (self.joints)))
+            for joint in self.joints:
+                fw (b"%s\0" % bytes (joint.name, 'UTF-8'))
+
+                if joint.parent is not None:
+                    local_transform = joint.parent.matrix_local.inverted () @ joint.matrix_local
+                else:
+                    local_transform = joint.matrix_local
+
+                fw (struct.pack ("<ffff", *local_transform[0]))
+                fw (struct.pack ("<ffff", *local_transform[1]))
+                fw (struct.pack ("<ffff", *local_transform[2]))
+                fw (struct.pack ("<ffff", *local_transform[3]))
+
+                if joint.parent is not None:
+                    fw (struct.pack ("<h", self.name_to_joint_id[joint.parent.name]))
+                else:
+                    fw (struct.pack ("<h", -1))
+
+    def WriteText (self, filename : str):
+        with open (filename, "wb") as file:
+            fw = file.write
+            fw (b"[1]\n\n")	# Version
+            fw (b"joint_count %u\n" % len (self.joints))
+            fw (b"vertex_count %u\n" % len (self.verts))
+            fw (b"triangle_count %u\n\n" % len (self.tris))
+            fw (b"joints:\n")
+
+            for joint in self.joints:
+                fw (b"%s\n" % bytes (joint.name, 'UTF-8'))
+                if joint.parent is not None:
+                    local_transform = joint.parent.matrix_local.inverted () @ joint.matrix_local
+                else:
+                    local_transform = joint.matrix_local
+
+                fw (b"%.6f %.6f %.6f %.6f\n" % local_transform[0][:])
+                fw (b"%.6f %.6f %.6f %.6f\n" % local_transform[1][:])
+                fw (b"%.6f %.6f %.6f %.6f\n" % local_transform[2][:])
+                fw (b"%.6f %.6f %.6f %.6f\n" % local_transform[3][:])
+
+                if joint.parent is not None:
+                    fw (b"%u\n\n" % self.name_to_joint_id[joint.parent.name])
+                else:
+                    fw (b"-1\n\n")
+
+            fw (b"vertices:\n")
+            for vert in self.verts:
+                fw (b"%.6f %.6f %.6f\n" % vert.position)
+                fw (b"%.6f %.6f %.6f\n" % vert.normal)
+                fw (b"%.6f %.6f %.6f\n" % vert.weights)
+                fw (b"%i %i %i %i\n" % vert.joint_id)
+                fw (b"\n")
+
+            fw (b"triangles:\n")
+            for tri in self.tris:
+                fw (b"%u %u %u\n" % tuple (tri))
+
+def ExportMeshes (
+    context : bpy.types.Context,
+    filename : str,
+    use_selection : bool,
+    apply_transform : bool,
+    axis_conversion_matrix : mathutils.Matrix
+):
+    import os
+
+    if bpy.ops.object.mode_set.poll ():
+        bpy.ops.object.mode_set (mode = 'OBJECT')
+
+    if use_selection:
+        objs = context.selected_objects
+    else:
+        objs = context.scene.objects
+
+    for obj in objs:
+        try:
+            me = obj.to_mesh ()
+        except RuntimeError:
+            continue
+
+        armature_obj = obj.find_armature ()
+        armature = bpy.types.Armature = None
+        if armature_obj is not None:
+            armature = armature_obj.data.copy ()
+
+        # Apply object transform and calculate normals
+        if apply_transform:
+            me.transform (obj.matrix_world)
+            if armature is not None:
+                armature.transform (obj.matrix_world)
+
+        if axis_conversion_matrix is not None:
+            me.transform (axis_conversion_matrix.to_4x4 ())
+            if armature is not None:
+                armature.transform (axis_conversion_matrix.to_4x4 ())
+
+        me.calc_normals ()
+
+        # Triangulate mesh
+        bm = bmesh.new ()
+        bm.from_mesh (me)
+        bmesh.ops.triangulate (bm, faces = bm.faces[:])
+        bm.to_mesh (me)
+        bm.free ()
+
+        result = Mesh.FromMeshAndArmature (obj, me, armature)
+        output_filename = os.path.join (os.path.dirname (filename), obj.name) + Exporter.filename_ext
+        result.WriteBinary (output_filename)
+        obj.to_mesh_clear ()
+        print (f"Exported mesh {obj.name} to file {output_filename}.\n")
+
+@orientation_helper (axis_forward = '-Z', axis_up = 'Y')
+class Exporter (bpy.types.Operator, ExportHelper):
+    """Export mesh data"""
+    bl_idname = "export.anim_example_mesh"
+    bl_label = "Export mesh with skinning (.mesh)"
+    bl_options = { 'REGISTER', 'UNDO' }
+    filename_ext = ".mesh"
+
+    use_selection : BoolProperty (
+        name = "Only Selected",
+        description = "Export only the selected meshes.",
+        default = True
+    )
+
+    apply_transform : BoolProperty (
+        name = "Apply object transform",
+        description = "Apply the object transform matrix when exporting meshes.",
+        default = True
+    )
+
+    def execute (self, context : bpy.types.Context):
+        context.window.cursor_set ('WAIT')
+        ExportMeshes (
+            context,
+            self.filepath,
+            self.use_selection,
+            self.apply_transform,
+            axis_conversion (to_forward = self.axis_forward, to_up = self.axis_up)
+        )
+        context.window.cursor_set ('DEFAULT')
+
+        return { 'FINISHED' }
+
+def export_menu_func (self, context : bpy.types.Context):
+    self.layout.operator (Exporter.bl_idname)
